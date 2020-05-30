3d5dc76bffcee919249c1200a4d1d88876e11129
Arsen <ar.poryadin@yandex.ru>
2020-05-30 21:13:24 +0800

    inception

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_X6iAXX7Q0',
+        userPoolClientId: '7uan4p8ldmtuuqcvvv77jd61ac',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://kcjihhoe69.execute-api.us-east-1.amazonaws.com/prod'
+    }
+};
